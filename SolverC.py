a5eb9b8fc9c39aa41ee2c5aabff05a25fab04cd8
Stijn Verdenius <stijn@verdenius.com>
2017-06-24 15:46:35 +0200

    opdracht c

--- SolverC.py
+++ SolverC.py
@@ -13,84 +13,20 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from matplotlib import cm
 from copy import deepcopy
 
-def solverC(houseList, boardLength, boardHeight, wirePrice, batteryOptions, maxOvercapacity,batterycosts, method = "A"):
+def solverC(houseList, boardLength, boardHeight, wirePrice, batteryOptions, batterycosts, batteryConfiguration):
 	
-	checked = set()
-	
-	# precalculate capacity
-	totalCapacity = 0
-	for house in houseList:
-		totalCapacity += house.netto
+	# new configuration to be checked
+	readableConfiguration = countBatteries(batteryConfiguration, batteryOptions)
+	wireCost = 0
+	batteryCost = 0
+	# calculate wirecost
+	wireCost = wireCost1(100, batteryConfiguration, boardLength,boardHeight, houseList, wirePrice, 0, readableConfiguration)
+	# calculate batterycost
+	batteryCost = batteryCost1(batteryOptions, batterycosts, batteryConfiguration)
+	# calculate total costafter
+	cost = wireCost + batteryCost
 
-	possibleConfigurations = []
-
-	for x in range(34):
-		for y in range(9):
-			cap = x*batteryOptions[0] + y*batteryOptions[1]
-			if (cap >totalCapacity*1.4) and (cap < totalCapacity*1.5):
-				confList = [batteryOptions[0] for i in range(x)] + [batteryOptions[1] for j in range(y)]
-				possibleConfigurations.append(confList)
-
-	
-	# make inital configuration
-	# batteryConfiguration = initialConfiguration(method, totalCapacity, maxOvercapacity, batteryOptions)
-	
-	# calculate initial cost
-	# batteryCost = batteryCost1(batteryOptions, batterycosts, batteryConfiguration)
-	# wireLength = 0
-	# cap = 1
-	# while(cap > 0):
-	# 	print "initiation try"
-	# 	batteryList = createBatteryList(batteryConfiguration,boardLength,boardHeight, houseList)
-	# 	cap, wireLength, itt = SolverB.solverB(houseList, batteryList,boardLength,boardHeight)
-	# costBefore = batteryCost + wireLength*wirePrice 
-	# print "initiation confirmed : ", countBatteries(batteryConfiguration, batteryOptions), len(batteryConfiguration)
-	results = {}
-
-	# do until converge
-	# nothingChanged = 0
-	# while(nothingChanged < 10):
-	for batteryConfiguration in possibleConfigurations:
-
-		# change something
-		# oldConfiguration = deepcopy(batteryConfiguration)
-		# batteryConfiguration = changeConfiguration(batteryConfiguration, batteryOptions)
-
-		# if capacity is too high or low try something else
-		# if((sum(batteryConfiguration)>(totalCapacity*maxOvercapacity*1.1)) or (sum(batteryConfiguration)<=(totalCapacity)) or (tuple(countBatteries(batteryConfiguration, batteryOptions)) in checked)):
-		# 	batteryConfiguration = oldConfiguration
-		# 	continue
-
-		# new configuration to be checked
-		readableConfiguration = countBatteries(batteryConfiguration, batteryOptions)
-		# checked.add(tuple(readableConfiguration))
-		# print readableConfiguration, len(batteryConfiguration)
-
-		wireCost = 0
-		batteryCost = 0
-		# costAfter = 0
-		# calculate cost after
-		# calculate wirecost
-		wireCost = wireCost1(25, batteryConfiguration, boardLength,boardHeight, houseList, wirePrice, 0, readableConfiguration)
-		# calculate batterycost
-		batteryCost = batteryCost1(batteryOptions, batterycosts, batteryConfiguration)
-		# calculate total costafter
-		cost = wireCost + batteryCost
-
-		results[str(readableConfiguration)] = cost
-		
-		# print costBefore- costAfter
-
-		# if no improvement, swap back, else keep change
-		# if (costAfter >= costBefore):
-		# 	print "worse so restore:", countBatteries(oldConfiguration, batteryOptions)
-		# 	batteryConfiguration = oldConfiguration
-		# 	nothingChanged += 1
-		# else:
-		# 	print "better so use : ", readableConfiguration
-		# 	costBefore = costAfter
-		# 	nothingChanged = 0
-	return results
+	return cost, batteryCost
 
 def createBatteryList(configuration, l, h, houseList):
 	#create batteryList
@@ -148,12 +84,13 @@ def wireCost1(iterations, batteryConfiguration, boardLength,boardHeight, houseLi
 	while(len(wireCosts) < iterations):
 		if (tries > 200):
 			return 1000000000000000000000
-		print "solver b", "nothing canged ", len(wireCosts), " batteries : ", countlist
+		print ('solver b : found samples [%d%%]\r'%len(wireCosts)),
 		batteryList = createBatteryList(batteryConfiguration,boardLength,boardHeight, houseList)
 		cap, wireLength, itt = SolverB.solverB(houseList, batteryList,boardLength,boardHeight)
 		if (cap == 0):
 			wireCosts.append(wireLength*wirePrice)
+			tries = 0
+		tries += 1
 	wireCost = np.mean(wireCosts)
 
-	print "in formula", wireCost
 	return wireCost
--- check.py
+++ check.py
@@ -1,9 +1,15 @@
-check = []
-		for i in range(len(houseList)):
-			check.append(0)
-			house = houseList[i]
-			for battery in batteryList:
-				if (battery.assignedHouses[house.name][1]):
-					check[i] += 1
+# check = []
+# 		for i in range(len(houseList)):
+# 			check.append(0)
+# 			house = houseList[i]
+# 			for battery in batteryList:
+# 				if (battery.assignedHouses[house.name][1]):
+# 					check[i] += 1
 
-		print "before ", check.count(1)
+# 		print "before ", check.count(1)
+
+import time
+
+for i in range(100):
+    time.sleep(0.1)
+    print 'Downloading File FooFile.txt [%d%%]\r'%i,

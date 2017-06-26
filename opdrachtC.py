dd4e284d75f4a13a18625d074e2d6c960787853d
Stijn Verdenius <stijn@verdenius.com>
2017-06-26 17:11:33 +0200

    last monday

--- opdrachtC.py
+++ opdrachtC.py
@@ -12,11 +12,70 @@ import numpy as np
 import SolverB
 import SolverC
 import pickle
-
-
+from copy import deepcopy
 
 
 def main():
+
+	# with open("finalBoard3_CanalyseConstructiefJasperr.csv", "r") as f:
+	# 	f.readline()
+	# 	for line in f:
+	# 		print line
+	# 		[float(x) for x in line.split(",")[1:]]
+
+	# cs1 = np.genfromtxt("finalBoard1_CanalyseConstructiefJasper.csv", delimiter=",", dtype=None)
+	cs2 = np.loadtxt("finalBoard2_CanalyseConstructiefJasperrr.csv",dtype=float,delimiter=',',skiprows=1,usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13))
+	cs3 = np.loadtxt("finalBoard3_CanalyseConstructiefJasperr.csv",dtype=float,delimiter=',',skiprows=1,usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13))
+
+	cs1 = np.loadtxt("finalBoard1_CanalyseConstructiefJasper.csv",dtype=float,delimiter=',',skiprows=1,usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13))
+	conf = np.loadtxt("finalBoard2_CanalyseConstructiefJasperrr.csv",dtype="str",delimiter=',',skiprows=1,usecols=(0))
+	results = []
+	# for c in conf:
+	# print cs2
+
+	# print len(conf), 46
+	for i in range(len(cs1[0])):
+		score1 = np.argsort(-1*cs1[:,i])
+		score2 = np.argsort(-1*cs2[:,i])
+		score3 = np.argsort(-1*cs3[:,i])
+		scoretotal = [0 for x in range(len(score1))]
+		# print score1
+
+		for j in range(len(score1)):
+
+			scoretotal[score1[j]] += j
+			scoretotal[score2[j]] += j
+			scoretotal[score3[j]] += j
+
+		scoretotal = np.array(scoretotal)/3
+		results.append(scoretotal)
+		# print scoretotal
+
+
+	# print np.array(results).T
+	ratios = [conf,[],[]]
+	for c in conf:
+		ratio1 = 0
+		ratio1 = (float(c[-1]))/(int(c[0])+int(c[-1]))
+		ratio2 = (float(c[-1])*1800)/((int(c[0])*450)+(int(c[-1])*1800))
+		ratios[1].append(ratio1)
+		ratios[2].append(ratio2)
+
+	output = results
+
+	output =  np.array(output).T
+	g = open("wireLengthResults.csv", "w")
+	g.write("0,5,6,7,8,9,10,11,12,13,14,15,100\n")
+	for y in output:
+		stri = ""
+		for x in y:
+			stri = stri + str(x) + ","
+		stri = stri + "\n"
+		g.write(stri)
+
+
+
+def main2():
 	# boardNames = ["small_test_board20x20_24h"]#["finalBoard1", "finalBoard2", "finalBoard3"]
 	boardNames = ["finalBoard1", "finalBoard2", "finalBoard3"]
 
